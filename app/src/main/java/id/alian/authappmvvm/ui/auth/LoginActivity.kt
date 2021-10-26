5c582b4e36b84e756fe0c14234e854deff51dec0
alianhakim8 <alianhakim8@gmail.com>
2021-10-26 16:33:09 +0700

    setting up first time project & create basic layout

---
+++ app/src/main/java/id/alian/authappmvvm/ui/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package id.alian.authappmvvm.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.alian.authappmvvm.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/id/alian/authappmvvm/ui/auth/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package id.alian.authappmvvm.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.alian.authappmvvm.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
